7d750923deb750f50985b2240e0ff46fa8a9bb49
лев елистратов <msijkeee@gmail.com>
2023-03-27 22:38:22 +0700

    module4-task1 - fixes 2.0 - final

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 import CityCard from '../city-card/city-card';
-import { Offers } from '../../types/offers';
+import { Offer } from '../../types/offers';
 
 
 type OffersListType = {
-  offers: Offers;
+  offers: Offer[];
 };
 
 function OffersList({offers }: OffersListType): JSX.Element {
   const [activeId, setActiveId] = useState('0');
   return (
     <div className="cities__places-list places__list tabs__content" key={activeId}>
-      {offers.map((offers) => (
+      {offers.map((offer) => (
         <CityCard
-          key={`${offers.id}`}
-          offer={offers}
-          onMouseOverHandler={() => setActiveId(offers.id)}
+          key={`${offer.id}`}
+          offer={offer}
+          onMouseOverHandler={() => setActiveId(offer.id)}
         />
       ))}
     </div>
